fbec624cfe53a3f2199be15bf82d1e08d907857a
Evan Madill <madille@myumanitoba.ca>
2022-05-14 23:52:42 -0500

    build: vscode configs, intial next config

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # evanm.io
+
 Portfolio website running on Next.js + Strapi
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const baseConfig = {
+    poweredByHeader: false,
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en-US"],
+        defaultLocale: "en-US"
+    },
+    // Using env as config since runtime config isn't supported by serverless deployments
+    env: {
+        url: "https://evanm.io",
+        siteName: "evanm.io",
+        siteDisplayName: "Evan Madill's Portfolio Website",
+        description: "The frontend component of a portfolio website running Next.js",
+        locale: "en_US"
+    }
+};
+
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            /* development only config options here */
+            ...defaultConfig,
+            ...baseConfig
+        };
+    }
+
+    return {
+        ...defaultConfig,
+        ...baseConfig
+    };
 };
